c03e4a14f7cd6f2e495525ebd52e8536b5a24e17
Raghunandan <16raghunandangupta@gmail.com>
2024-01-20 23:43:35 +0530

    MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/expenses.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       home: Expenses(),
-    ),
-  );
+    );
+  }
 }
